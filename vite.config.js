e6978cff1058f0e79b500ed7140cf4984850c862
PinSpuMS <pin.ch@spulive.net>
2024-11-13 08:28:23 +0700

    edit url path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/2566-02-csi205-multiPagesVerNav_/'
+  base: '/2566-02-csi205-multipagesVerNav_/'
 })
